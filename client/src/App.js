f96e0fef8481a12e4317dd90ae67b3d68e3a9394
Nitesh <niteshtiwari5222@gmail.com>
2023-03-11 14:12:38 +0530

    deployment update 10

--- client/src/App.js
+++ client/src/App.js
@@ -45,118 +45,114 @@ function App() {
   const ThemeColor = useSelector((state) => state.setColorReducer.themeColor);
   const rgb = ThemeColor.split(")")[0].split("(")[1];
 
-
   const lightTheme = {
     colors: {
-      "heading": "rgb(24 24 29)",
-      "heading2": "rgb(255, 255, 255)",
-      "white": "#fff",
-      "black": " #212529",
-      "cyan": "#1ca9fe",
-      "green": "#4eac6d",
-      "danger": "#ff4e2b",
-      "light": "#223645",
-      "primaryRgb": `${ThemeColor}`,
+      heading: "rgb(24 24 29)",
+      heading2: "rgb(255, 255, 255)",
+      white: "#fff",
+      black: " #212529",
+      cyan: "#1ca9fe",
+      green: "#4eac6d",
+      danger: "#ff4e2b",
+      light: "#223645",
+      primaryRgb: `${ThemeColor}`,
 
-
-      text:{
-        "primary": "#000000",
-        "secondary": "rgba(29 ,29, 29, .8)",
-      },
-
-      "rgb":{
-      "primary": `${rgb}`,
-      "secondary": "78,172,109",
-      "cyan": "28,157,234",
-      "heading": "0,0,0",
+      text: {
+        primary: "#000000",
+        secondary: "rgba(29 ,29, 29, .8)",
       },
 
-      "bg":{
-        "primary": "#fff",
-        "secondary": "#eff7fe",
-      },
-      bg2:{
-        "primary": "#fff",
-        "secondary": "rgba(28,157,234,.05)",
+      rgb: {
+        primary: `${rgb}`,
+        secondary: "78,172,109",
+        cyan: "28,157,234",
+        heading: "0,0,0",
       },
 
-      btn:{
-      "primary": `${rgb}`,
-      "secondary": "22 163 74",
-      "danger": "255, 78, 43",
-      "light": "#f6f6f9",
+      bg: {
+        primary: "#fff",
+        secondary: "#eff7fe",
       },
-      border2:{
-         "primary": "#00000026"
+      bg2: {
+        primary: "#fff",
+        secondary: "rgba(28,157,234,.05)",
       },
-      boxShadow:{
-        "primary": "rgba(28, 157, 234, 0.2)",
+
+      btn: {
+        primary: `${rgb}`,
+        secondary: "22 163 74",
+        danger: "255, 78, 43",
+        light: "#f6f6f9",
       },
-      
-      "hr": "#ffffff",
-      "border": "239, 241, 242",
-      "img_border": "255, 255, 255",
-      "gradient": "linear-gradient(145deg,#1ca9fe,#1c6ee9);",
+      border2: {
+        primary: "#00000026",
+      },
+      boxShadow: {
+        primary: "rgba(28, 157, 234, 0.2)",
+      },
+
+      hr: "#ffffff",
+      border: "239, 241, 242",
+      img_border: "255, 255, 255",
+      gradient: "linear-gradient(145deg,#1ca9fe,#1c6ee9);",
     },
     media: {
       mobile: "800px",
       tab: "998px",
     },
   };
- const darkTheme = {
+  const darkTheme = {
     colors: {
-      "heading": "rgb(255, 255, 255)",
-      "heading2": "rgb(24 24 29)",
-      "white": "#ffffff",
-      "black": "#000000",
-      "cyan": "#1ca9fe",
-      "green": "#4eac6d",
-      "danger": "#ff4e2b",
-      "light": "#223645",
-      "primaryRgb": `${ThemeColor}`,
+      heading: "rgb(255, 255, 255)",
+      heading2: "rgb(24 24 29)",
+      white: "#ffffff",
+      black: "#000000",
+      cyan: "#1ca9fe",
+      green: "#4eac6d",
+      danger: "#ff4e2b",
+      light: "#223645",
+      primaryRgb: `${ThemeColor}`,
 
-      text:{
-        "primary": "#212529",
-        "secondary": "#8f9198",
+      text: {
+        primary: "#212529",
+        secondary: "#8f9198",
       },
 
-      "rgb":{
-      "primary": "0, 128, 201",
-      "secondary": "78,172,109",
-      "cyan": "28,157,234",
-      "heading": "255,255,255",
+      rgb: {
+        primary: "0, 128, 201",
+        secondary: "78,172,109",
+        cyan: "28,157,234",
+        heading: "255,255,255",
       },
 
-      "bg":{
-        "black": "#000000",
-        "primary": "#262626",
-        "secondary": "#2e2e2e",
+      bg: {
+        black: "#000000",
+        primary: "#262626",
+        secondary: "#2e2e2e",
       },
-      border2:{
-        "primary": "#FFFFFF26"
-     },
-     
-
-      "bg2":{
-        "primary": "#0c1631",
-        "secondary": "#0e1b38",
-      },
-      boxShadow:{
-        "primary": "rgba(1, 201 ,245, 0.4)",
+      border2: {
+        primary: "#FFFFFF26",
       },
 
-
-      btn:{
-      "primary": `${rgb}`,
-      "secondary": "22 163 74",
-      "danger": "255, 78, 43",
-      "light": "#25262c",
+      bg2: {
+        primary: "#0c1631",
+        secondary: "#0e1b38",
       },
-      
-      "hr": "#ffffff",
-      "border": "65, 66, 72",
-      "img_border": "31, 41, 55",
-      "gradient": "linear-gradient(145deg,#1ca9fe,#1c6ee9);",
+      boxShadow: {
+        primary: "rgba(1, 201 ,245, 0.4)",
+      },
+
+      btn: {
+        primary: `${rgb}`,
+        secondary: "22 163 74",
+        danger: "255, 78, 43",
+        light: "#25262c",
+      },
+
+      hr: "#ffffff",
+      border: "65, 66, 72",
+      img_border: "31, 41, 55",
+      gradient: "linear-gradient(145deg,#1ca9fe,#1c6ee9);",
     },
     media: {
       mobile: "800px",
@@ -210,7 +206,7 @@ function App() {
               <Route path="" element={<Login />} />
               <Route path="signup" element={<Signup />} />
             </Route>
-            <Route path="/team" element={<Error />} />
+            <Route path="*" element={<Error />} />
           </Routes>
         )}
       </div>
