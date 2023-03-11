f1ffe6c690d04c6ad34e83cfc7a1193fe3779778
Nitesh <niteshtiwari5222@gmail.com>
2023-03-11 13:59:58 +0530

    deployment update 9

--- client/src/Components/HeroSection.js
+++ client/src/Components/HeroSection.js
@@ -3,8 +3,7 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Button } from "../Styles/Button";
 import { HiChevronDoubleRight } from "react-icons/hi";
-import { SquareDonut, Donut, Circle, PolygonCard } from "react-awesome-shapes";
-
+// import { SquareDonut, Donut, Circle, PolygonCard } from "react-awesome-shapes";
 
 const HeroSection = () => {
   return (
@@ -42,7 +41,7 @@ const HeroSection = () => {
         </div>
       </div>
 
-      <div className="shapes">
+      {/* <div className="shapes">
         <div className="shape-1">
           <Circle
             color="linear-gradient(135deg, #a5b4fc, #6366f1)"
@@ -51,7 +50,7 @@ const HeroSection = () => {
           />
         </div>
         <div className="shape-2">
-        <Circle
+          <Circle
             color="linear-gradient(135deg, #a5b4fc, #6366f1)"
             size={["800px", "800px", "800px", "800px"]}
             zIndex={2}
@@ -77,7 +76,7 @@ const HeroSection = () => {
         <div className="shape-6">
           <SquareDonut size="50px" zIndex={2} color="#2a9bd1" />
         </div>
-      </div>
+      </div> */}
     </Wrapper>
   );
 };
@@ -161,37 +160,37 @@ const Wrapper = styled.section`
     height: 100%;
     animation: Zoom-fade 5s infinite linear;
     div {
-    position: absolute;
-  }
-  .shape-1 {
-    top: -10%;
-    left: -3%;
-    opacity: 0.1;
-  }
-  .shape-2 {
-    top: 40%;
-    right: 30%;
-    opacity: 0.1;
-  }
-  .shape-3{
-   top: 70%;
-   left: -3%;
-   transform-origin: center;
-   transform: rotate(20deg);
-   opacity: 0.1;
-  }
-  .shape-5 {
-    bottom: 0%;
-    left: 20%;
-    transition: all 0.5s;
-    animation: balloonfly-02 12s infinite;
-  }
-  .shape-6 {
-    bottom: 0%;
-    right: 20%;
-    transition: all 0.5s;
-    animation: balloonfly-01 12s infinite;
-  }
+      position: absolute;
+    }
+    .shape-1 {
+      top: -10%;
+      left: -3%;
+      opacity: 0.1;
+    }
+    .shape-2 {
+      top: 40%;
+      right: 30%;
+      opacity: 0.1;
+    }
+    .shape-3 {
+      top: 70%;
+      left: -3%;
+      transform-origin: center;
+      transform: rotate(20deg);
+      opacity: 0.1;
+    }
+    .shape-5 {
+      bottom: 0%;
+      left: 20%;
+      transition: all 0.5s;
+      animation: balloonfly-02 12s infinite;
+    }
+    .shape-6 {
+      bottom: 0%;
+      right: 20%;
+      transition: all 0.5s;
+      animation: balloonfly-01 12s infinite;
+    }
   }
   @keyframes zoom-fade {
     0% {
--- client/src/Components/Technologies.js
+++ client/src/Components/Technologies.js
@@ -1,37 +1,41 @@
 import React from "react";
 import styled from "styled-components";
 import { technologiesImg } from "../config.js/data";
-import { SquareDonut, Donut, Circle, PolygonCard } from "react-awesome-shapes";
+// import { SquareDonut, Donut, Circle, PolygonCard } from "react-awesome-shapes";
 
 const Technologies = () => {
- 
   return (
     <Wrapper className="technologies-section" id="technologies">
       <div className="custom-container">
-
-        <div className="flex items-start justify-center" data-aos="fade-up" data-aos-delay="200">
+        <div
+          className="flex items-start justify-center"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        >
           <div className="section-header text-center">
-             <h5>POWERFUL</h5>
+            <h5>POWERFUL</h5>
             <h2 className="capitalize">Techonolgies Used</h2>
           </div>
         </div>
 
         <div className="technologies-list">
           <ul>
-           {
-            technologiesImg.map((item, index)=>(
-              <li data-aos="fade-up" data-aos-delay={(index+1)*100} key={item.id}>
-              <div className="flex flex-col justify-center items-center p-8">
-              <img src={item.src} alt="tech-img" />
-              <h5 className="mt-5">{item.name}</h5>
-              </div>
-            </li>
-            ))
-           }
+            {technologiesImg.map((item, index) => (
+              <li
+                data-aos="fade-up"
+                data-aos-delay={(index + 1) * 100}
+                key={item.id}
+              >
+                <div className="flex flex-col justify-center items-center p-8">
+                  <img src={item.src} alt="tech-img" />
+                  <h5 className="mt-5">{item.name}</h5>
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
-      <div className="shapes">
+      {/* <div className="shapes">
         <div className="shape-1">
           <Circle
             color="linear-gradient(135deg, #a5b4fc, #6366f1)"
@@ -40,7 +44,7 @@ const Technologies = () => {
           />
         </div>
         <div className="shape-2">
-        <Circle
+          <Circle
             color="linear-gradient(135deg, #a5b4fc, #6366f1)"
             size={["500px", "500px", "500px", "500px"]}
             zIndex={2}
@@ -66,7 +70,7 @@ const Technologies = () => {
         <div className="shape-6">
           <SquareDonut size="50px" zIndex={2} color="#2a9bd1" />
         </div>
-      </div>
+      </div> */}
     </Wrapper>
   );
 };
@@ -92,7 +96,7 @@ const Wrapper = styled.section`
       h2 {
         font-size: 2.5rem;
         font-weight: 700;
-        color: ${({ theme }) => theme.colors.heading};;
+        color: ${({ theme }) => theme.colors.heading};
         margin: 25px 0;
       }
     }
@@ -105,23 +109,24 @@ const Wrapper = styled.section`
         flex-wrap: wrap;
         justify-content: center;
         li {
-         div{
-          background-color: ${({ theme }) => theme.colors.bg2.secondary};
-          border-width: 1px 1px 1px 1px;
-          border-color: ${({ theme }) => theme.colors.border2.primary};
-          width: 10rem;
-          height: 10rem;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          margin: 0 15px 50px;
-          border-radius: 10px;
-          transition: all 0.5s;
-          &:hover {
-        box-shadow: 0px 4px 24px ${({ theme }) => theme.colors.boxShadow.primary};
-        transform: scale(1.1);
-         }
-      }
+          div {
+            background-color: ${({ theme }) => theme.colors.bg2.secondary};
+            border-width: 1px 1px 1px 1px;
+            border-color: ${({ theme }) => theme.colors.border2.primary};
+            width: 10rem;
+            height: 10rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 0 15px 50px;
+            border-radius: 10px;
+            transition: all 0.5s;
+            &:hover {
+              box-shadow: 0px 4px 24px
+                ${({ theme }) => theme.colors.boxShadow.primary};
+              transform: scale(1.1);
+            }
+          }
           img {
             max-width: 100%;
             height: auto;
@@ -129,7 +134,6 @@ const Wrapper = styled.section`
         }
       }
     }
-
   }
 
   .shapes {
@@ -140,37 +144,37 @@ const Wrapper = styled.section`
     animation: Zoom-fade 5s infinite linear;
     z-index: 1;
     div {
-    position: absolute;
-  }
-  .shape-1 {
-    top: -10%;
-    left: -3%;
-    opacity: 0.1;
-  }
-  .shape-2 {
-    top: -15%;
-    right: 16%;
-    opacity: 0.1;
-  }
-  .shape-3{
-   top: 70%;
-   left: -3%;
-   transform-origin: center;
-   transform: rotate(20deg);
-   opacity: 0.1;
-  }
-  .shape-5 {
-    bottom: 0%;
-    left: 20%;
-    transition: all 0.5s;
-    animation: balloonfly-02 12s infinite;
-  }
-  .shape-6 {
-    bottom: 0%;
-    right: 20%;
-    transition: all 0.5s;
-    animation: balloonfly-01 12s infinite;
-  }
+      position: absolute;
+    }
+    .shape-1 {
+      top: -10%;
+      left: -3%;
+      opacity: 0.1;
+    }
+    .shape-2 {
+      top: -15%;
+      right: 16%;
+      opacity: 0.1;
+    }
+    .shape-3 {
+      top: 70%;
+      left: -3%;
+      transform-origin: center;
+      transform: rotate(20deg);
+      opacity: 0.1;
+    }
+    .shape-5 {
+      bottom: 0%;
+      left: 20%;
+      transition: all 0.5s;
+      animation: balloonfly-02 12s infinite;
+    }
+    .shape-6 {
+      bottom: 0%;
+      right: 20%;
+      transition: all 0.5s;
+      animation: balloonfly-01 12s infinite;
+    }
   }
 
   @media only screen and (min-width: 1680px) {
@@ -182,5 +186,4 @@ const Wrapper = styled.section`
       margin-left: auto;
     }
   }
-
 `;
