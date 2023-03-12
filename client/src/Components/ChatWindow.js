b71650bdbf1cbb323471b446e4cb0fdee6d02799
Nitesh <niteshtiwari5222@gmail.com>
2023-03-12 09:47:35 +0530

    deployment update 23

--- client/src/Components/ChatWindow.js
+++ client/src/Components/ChatWindow.js
@@ -28,7 +28,7 @@ import io from "socket.io-client";
 import { useRef } from "react";
 import { clearSelectChatAction } from "../Redux/Reducer/Chat/chat.action";
 
-const ENDPOINT = "https://e-talk-server.vercel.app";
+const ENDPOINT = "https://e-talk-server.vercel.app/";
 var socket, selectedChatCompare;
 
 const ChatWindow = () => {
