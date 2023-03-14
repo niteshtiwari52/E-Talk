04d66c0b74fe796c9ca0f4800fad61459e2c7885
Nitesh <niteshtiwari5222@gmail.com>
2023-03-14 16:46:53 +0530

    deployment update 30

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ server.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "*",
+    origin: "https://e-talk-client.vercel.app",
     methods: ["GET", "POST"],
   },
 });
